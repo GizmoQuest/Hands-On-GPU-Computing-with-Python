720339de42f1a25b45e0a751b3e84ada1be25edb
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2019-07-01 08:30:04 +0530

    Create HIPify-installer.sh

---
+++ Working with ROCm and PyOpenCL (Both AMD and NVIDIA)/7.1. Understanding How ROCm-C-C++ Works with HIPify,  HIP and OpenCL/HIPify-installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#HIPify - a unique tool from ROCm to easily convert CUDA code into HIP. 
+#1. First, install all dependencies to install and run hipify:
+sudo apt install git cmake clang-6.0 libclang-6.0-dev libclangperl libclanlib-dev libclang-dev zlib1g-dev
+cd /opt/
+git clone https://github.com/GPUOpen-ProfessionalCompute-Tools/HIP HIP
+cd HIP/hipify-clang/
+mkdir build dist
+cd build
+cmake \
+-DCMAKE_INSTALL_PREFIX=../dist \
+-DCMAKE_BUILD_TYPE=Release \
+..
+#2. Now, install hipify in the same build location:
+make -j install
+#3. Now, to set up the hipify command, use the following command:
+echo 'alias hipify=/opt/HIP/hipify-clang/dist/bin/./hipify-clang' >> ~/.bashrc
+#Reload the Bash shell:
+. ~/.bashrc
+#Run HIPify as "hipify CUDA-program.cu to HIP-program.cpp"
